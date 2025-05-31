f4691a703daa747ecf9ba012fa43f1e80de9ae6d
jezyq14 <michal.muzyka@proton.me>
2025-05-31 12:37:06 +0200

    Add schoolWebsiteUrl to configuration for environment variables

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,5 @@ import "dotenv/config";
 export default {
     token: process.env.TELEGRAM_BOT_TOKEN,
     chatId: process.env.TELEGRAM_CHAT_ID,
+    schoolWebsiteUrl: process.env.SCHOOL_WEBSITE_URL,
 };

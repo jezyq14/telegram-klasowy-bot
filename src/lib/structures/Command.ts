39cc0c74a0a20094b879d29d0afd5168f6d25bba
jezyq14 <michal.muzyka@proton.me>
2025-06-01 19:35:40 +0200

    Refactor Command class to use CommandData type for better structure and clarity

--- src/lib/structures/Command.ts
+++ src/lib/structures/Command.ts
@@ -2,16 +2,27 @@ import { Context } from "telegraf";
 
 import { client } from "../..";
 
+type CommandData = {
+    name: string;
+    description?: string;
+    usage?: string;
+    aliases?: string[];
+    category?: string;
+};
+
 export class Command {
     constructor(
-        public name: string,
+        public data: CommandData,
         public run: (ctx: Context) => Promise<void | any>
     ) {
-        client.command(name, async (ctx) => {
+        client.command(this.data.name, async (ctx) => {
             try {
                 await this.run(ctx);
             } catch (error) {
-                console.error(`Error executing command ${this.name}:`, error);
+                console.error(
+                    `Error executing command ${this.data.name}:`,
+                    error
+                );
                 await ctx.reply(
                     "Code broke, very sad. Buy 2003 Toyota Corolla to fix."
                 );

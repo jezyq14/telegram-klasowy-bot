37fa5c0f3e6c4ffe00b98f1b96bdd84e3a36e88e
jezyq14 <michal.muzyka@proton.me>
2025-06-01 19:35:48 +0200

    Refactor luckynumbers command initialization to use object syntax for improved clarity

--- src/commands/luckynumbers.ts
+++ src/commands/luckynumbers.ts
@@ -1,6 +1,6 @@
 import { Command, getLuckyNumbersMessage } from "../lib";
 
-new Command("luckynumbers", async (ctx) => {
+new Command({ name: "luckynumbers" }, async (ctx) => {
     const message = await getLuckyNumbersMessage();
 
     await ctx.reply(message);
